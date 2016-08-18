23bbffa468c04de1996578271894e14f0fa4b2aa
AssassinTG <AssassinTGBot@gmail.com>
2016-08-18 22:24:06 +0430

    Update AboutMe.lua

--- plugins/AboutMe.lua
+++ plugins/AboutMe.lua
@@ -1,53 +1,25 @@
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
 do
-
 local function run(msg, matches)
-  if matches[1] == 'bot' then
+  if matches[1] == 'me' then
     if is_sudo(msg) then
-    send_document(get_receiver(msg), "./data/me/version.webp", ok_cb, false)
-      return "B L A C K +\n---------------------------------------------\n|An Advanced Bot Based On #LUA|\n\n> Bot Number : +19092545429\n---------------------------------------------\n #Developer : [@MehdiHS | @MehdHS_Bot]\n---------------------------------------------\n #Sudoers :\n1 > [#Amirho3inf]\n2 > [#Surena]\n3 > [#Vandad_YflQw]\n4 > [#Alireza_NiGht]\n---------------------------------------------\n> Channel : @Black_CH"
+      return reply_msg(msg.id," You Are |—>> 😏Sudo😏 ",ok_cb,false)
     elseif is_admin1(msg) then
-    send_document(get_receiver(msg), "./data/me/admin.webp", ok_cb, false)
-      return "You're Admin"
+      return " You Are |—>> 🙃Admin🙃"
     elseif is_owner(msg) then
-    send_document(get_receiver(msg), "./data/me/owner.webp", ok_cb, false)
-      return "You're Owner"
+      return " You Are |—>> 🤑Owner🤑"
     elseif is_momod(msg) then
-    send_document(get_receiver(msg), "./data/me/mod.webp", ok_cb, false)
-      return "You're Moderator"
+      return " You Are |—>> 😎Promoted😎"
     else
-    send_document(get_receiver(msg), "./data/me/mmbr.webp", ok_cb, false)
-      return "You're Just member :("
+      return " You Are |—>> 🤓Member🤓 "
     end
   end
 end
 
 return {
   patterns = {
-    "^[#!/]([Bb]ot)$",
-    "^([Bb]ot)$"
+"^([Mm]e)$", 
+"^[#/!]([Mm]e)$"
     },
   run = run
 }
 end
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
